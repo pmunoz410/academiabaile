53a91bb6e4bf41ca67644590a246a7c102c02f93
Paul Muñoz <paul18a20@gmail.com>
2024-08-03 23:58:40 -0500

    Agregando gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+import { src, dest } from 'gulp'
+import * as dartSass from 'sass' 
+import gulpSass from 'gulp-sass'
+
+const sass = gulpSass( dartSass )
+
+export function css( done ) {
+    src( 'src/scss/app.scss' )
+        .pipe( sass() )
+        .pipe( dest( 'build/css' ) )
+    done()
+}
