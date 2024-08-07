0a9d2aebadaf5d5028d151112f8e978e95cbfa57
Paul Muñoz <paul18a20@gmail.com>
2024-08-06 23:48:33 -0500

    Sincronizando cambios de estilos SASS

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-import { src, dest } from 'gulp'
+import { src, dest, watch } from 'gulp'
 import * as dartSass from 'sass' 
 import gulpSass from 'gulp-sass'
 
@@ -6,7 +6,11 @@ const sass = gulpSass( dartSass )
 
 export function css( done ) {
     src( 'src/scss/app.scss' )
-        .pipe( sass() )
+        .pipe( sass().on('error', sass.logError) )
         .pipe( dest( 'build/css' ) )
     done()
+}
+
+export function dev() {
+    watch( 'src/scss/**/*.scss', css )
 }
